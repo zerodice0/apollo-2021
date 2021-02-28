8bf7a0f81e06ea1877dcfb7809f269c317e3a998
Sung-jun Um <zerodice0@gmail.com>
2021-02-28 21:48:25 +0900

    until lecture #1.3

---
+++ src/routes/Home.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+import styled from 'styled-components'
+import Movie from '../components/Movie'
+
+const GET_MOVIES = gql`
+  { 
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+const _home = () => {
+  const {loading, data} = useQuery(GET_MOVIES)
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2021</Title>
+        <Subtitle>Working with graphQL</Subtitle>
+      </Header>
+    {loading 
+      && <Loading>Loading...</Loading>}
+      <Movies>
+      {!loading
+        && data.movies
+        && data.movies.map(
+          m => (
+              <Movie id={m.id} bg={m.medium_cover_image}/>
+              )
+            )
+          }
+      </Movies>
+    </Container>
+  )
+}
+
+export default _home
