bd6e6c13679fc8f1c0ea5cdcfb2696369da1ba67
Sung-jun Um <zerodice0@gmail.com>
2021-03-04 22:26:25 +0900

    add Query (TOGGLE_LIKE_MOVIE). add import (useMutation, gql). fix some layout. (add like/unlike button.)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,31 +1,59 @@
+import { useMutation } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 import React from 'react'
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
+const TOGGLE_LIKE_MOVIE = gql`
+  mutation toggleLikeMovie($id: Int!, $isLiked: Boolean!) {
+    toggleLikeMovie(id: $id, isLiked: $isLiked) @client
+  }
+`
+
 const Container = styled.div`
   height: 380px;
   width: 100%;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16),
-    0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
-  border-radius: 7px;
-
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 
 `
 const Poster = styled.div`
   background-image: url(${props => props.bg});
-  height: 100%;
   width: 100%;
+  height: 100%;
   background-size: cover;
   background-position: center center;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16),
+    0 3px 6px rgba(0, 0, 0, 0.23);
 `
 
-const _movie = ({id, bg}) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={bg}>
-      </Poster>
-    </Link>
-  </Container>)
+const Button = styled.button`
+  align-self: center;
+  color: tomato;
+`
+
+const SLink = styled(Link)`
+  display:flexbox;
+  width: 100%;
+  height:100%;
+`
+
+const _movie = ({id, bg, isLiked}) => {
+  const [toggleLikeMovie] = useMutation(TOGGLE_LIKE_MOVIE, {
+    variables: {id: parseInt(id), isLiked: isLiked}
+  })
+
+  return(
+    <Container>
+      <SLink to={`/${id}`}>
+        <Poster bg={bg}>
+        </Poster>
+      </SLink>
+      <Button onClick={toggleLikeMovie}>{isLiked ? "Unlike" : "like"}</Button>
+    </Container>
+  )
+}
 
 export default _movie
